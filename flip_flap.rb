4cdf9d752243a7f51c6747717da85e12804df1db
Hiraku <dev@hiraku.tw>
2015-10-10 18:32:01 +0800

    yaml_buddy

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,20 +1,10 @@
 require_relative 'tsv_buddy'
+require_relative 'yaml_buddy'
 require 'yaml'
 require 'json'
 
 class FlipFlap
   # Do NOT create an initialize method
   include TsvBuddy
-
-  # this method takes a Yaml string and create a data structure to put into `@data`.
-  # You should be able to do this with one line of code using the `yaml` library's load method.
-  def take_yaml(yml)
-    @data = JSON.parse(YAML.load(yml).to_json)
-  end
-
-  # this method should return `@data` in Yaml format.
-  def to_yaml
-    @data.to_yaml
-  end
-
+  include YamlBuddy
 end
